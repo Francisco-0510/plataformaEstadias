f0f6ca0542b8fc4a1770e830c91311caf458d516
Francisco-0510 <francisco109623@gmail.com>
2023-03-13 12:04:55 -0600

    Integración pages Forms Inscripción Estudiante

--- src/app/pages/splash-screen/splash-screen.page.ts
+++ src/app/pages/splash-screen/splash-screen.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash-screen',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splash-screen.page.scss'],
 })
 export class SplashScreenPage implements OnInit {
-
-  constructor() { }
+  tiempoReal = 3;
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.time();
   }
-
+  time() {
+    let intervalId = setInterval(() => {
+      this.tiempoReal = this.tiempoReal - 1;
+      console.log(this.tiempoReal);
+      if (this.tiempoReal === 0) {
+        clearInterval(intervalId);
+        this.router.navigate(['/login']);
+      }
+    }, 1000);
+  }
 }
