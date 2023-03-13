5155e06c972580001cbd8afa4f5469bb822f57e6
Francisco-0510 <francisco109623@gmail.com>
2023-03-13 10:44:36 -0600

    Maquetado de Login Estudiante

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  tiempoReal = 3;
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.time();
+  }
+  time() {
+    let intervalId = setInterval(() => {
+      this.tiempoReal = this.tiempoReal - 1;
+      console.log(this.tiempoReal);
+      if (this.tiempoReal === 0) {
+        clearInterval(intervalId);
+        this.router.navigate(['/login']);
+      }
+    }, 1000);
+  }
 }
